b33a0f26008268921ff2ef03a0183fc04febe281
Cesar Banegas <cesarfigueroa503@gmail.com>
2023-08-17 18:48:38 -0600

    Asi se va esta cosa

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -12,7 +12,7 @@ class UsuarioController extends Controller
         $responseTipoMembresia = Http::get('http://localhost:8080/api/tipomeb/obtener');
         $tipoMembresia = $responseTipoMembresia->json();
         
-        
+        $mensaje = "";
 
         /*
 
@@ -22,22 +22,54 @@ class UsuarioController extends Controller
 
         //
 
-        return view('sinup', compact('tipoMembresia'));
+        return view('sinup', compact('mensaje','tipoMembresia'));
     }
 
     public function validarCampos(Request $request){
-        
+        $responseTipoMembresia = Http::get('http://localhost:8080/api/tipomeb/obtener');
+        $tipoMembresia = $responseTipoMembresia->json();
+
         $nombre = $request->input('nombre');
         $apellido = $request->input('apellido');
         $email = $request->input('email');
         $password = $request->input('password');
         $confPassword = $request->input('confPassword');
         
-        if(!$request->filled('nombre')){
-            return redirect()->route('usuario.crear');
+        $valorSeleccionado = $request->input('tipo-de-membresia');
+
+        if(
+            !$request->filled('nombre') or 
+            !$request->filled('apellido') or
+            !$request->filled('email') or
+            !$request->filled('password') or
+            !$request->filled('confPassword')
+        )
+        {
+            $mensaje = "llenar todos los campor porfavor";
+            return view('sinup', compact('mensaje', 'tipoMembresia'));
         }
         else{
-            return view('musica');
+            if($password != $confPassword){
+                $mensaje = "Contrasenia no coincide con la confirmacion de contrasenia";
+                return view('sinup', compact('mensaje', 'tipoMembresia'));
+            }
+            if(!$request->filled('numeroTarjeta') or
+            !$request->filled('cvv')){
+                $mensaje = "Ingrese tarjeta y sus campos";
+                return view('sinup', compact('mensaje', 'tipoMembresia'));
+            }
+            return redirect()->route('usuario.guardar');
+            
         }
+    }
+
+    function guardarUsuario(Request $request){
+        $response = Http::post('http://localhost:8080/api/usuario/guardar', [
+            'nombre' => $request->input('nombre'),
+            'apellido' => $request->input('apellido'),
+            'email' => $request->input('email'),
+            'password'=>$request->input('password')
+        ]);
+        return redirect()->route('musica.index');
     }
 }
